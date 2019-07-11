86dc835ebe709e342f7d6df6088e521c50298b51
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-07-11 18:16:57 +0300

    added web controllers

--- web/src/main/java/net/coloboff/csvreader/web/controllers/AirportResource.java
+++ web/src/main/java/net/coloboff/csvreader/web/controllers/AirportResource.java
@@ -1,5 +1,53 @@
 package net.coloboff.csvreader.web.controllers;
 
+import net.coloboff.csvreader.core.CsvReader;
+import net.coloboff.csvreader.core.dto.AirportDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("api/v1/airport")
 public class AirportResource {
 
+    private final CsvReader csvReader;
+
+    public AirportResource(CsvReader csvReader) {
+        this.csvReader = csvReader;
+    }
+
+    /**
+     * Returns a list of all airports.
+     *
+     * @return
+     * @throws IOException
+     */
+
+    @GetMapping("/all")
+    public Collection<AirportDTO> getAll() throws IOException {
+        return csvReader.getAll();
+    }
+    /**
+     *Returns a list of filtered airports by the column value specified in the properties.
+     *
+     *@param value
+     *@return
+     *@throws IOException
+     */
+    @GetMapping("/filter")
+    public Collection<AirportDTO> filter(@RequestParam(name = "value") final  String value) throws IOException {
+        return csvReader.filter(value);
+    }
+
+    /**
+    * Returns all possible filters.
+    */
+    @GetMapping("/filters")
+    public Collection<String> getAllFilters() {
+        return csvReader.getAllFilters();
+    }
 }
